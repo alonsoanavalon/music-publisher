34ee8bd43bc3f8983f8f6cc74f5eb9f788e12e41
k3yzen <alonso.anavalon@gmail.com>
2022-04-24 16:57:06 -0400

    [Publisher] - [FEAT] - publisher controller created

--- src/publisher/publisher.controller.ts
+++ src/publisher/publisher.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('publisher')
-export class PublisherController {}
+export class PublisherController {
+  @Get()
+  sayHello(): string {
+    return 'Hello world from publisher';
+  }
+}
